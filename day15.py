a88fe5a03b834e880c728986f4318e9657b1acf7
Brian Wright <4408242+PennRobotics@users.noreply.github.com>
2022-12-15 22:37:41 +0100

    submit Part B solution

--- day15.py
+++ day15.py
@@ -21,38 +21,56 @@ dist_to_2MM = lambda v: abs(v[1]-2000000)
 
 class Endpoints:
     def __init__(self, sc, md):
+        self.intop = (sc[0], sc[1] - md)
+        self.inbot = (sc[0], sc[1] + md)
+        self.inlef = (sc[0] - md, sc[1])
+        self.inrig = (sc[0] + md, sc[1])
+        self.area = 2 * md**2
+
         self.top = (sc[0], sc[1] - md - 1)
         self.bot = (sc[0], sc[1] + md + 1)
         self.lef = (sc[0] - md - 1, sc[1])
         self.rig = (sc[0] + md + 1, sc[1])
 
     def __str__(self):
-        return f'Top: {self.top}, Left: {self.lef}, Bottom: {self.bot}, Right: {self.rig}'
+        return f'Top: {self.top}, Left: {self.lef}, Bottom: {self.bot}, Right: {self.rig}, Area: {self.area}'
 
     def check_point_contained(self, pt):
         # There's this trick:
-        #   1. Get the area of the top half triangle and bottom half triangle using four endpoints.
+        #   1. Get the area of the existing region (done during class init)
         #   2. Get the area of the four triangles made by connecting each adjacent endpoint pair and candidate point.
-        #   3. If these are equal, the point is contained. Otherwise, the point is outside the region.
-
-        # TODO
-        pass
+        #   3. If these are equal(-ish), the point is contained. Otherwise, the point is outside the region.
+        tri_area = lambda pA, pB, pC: abs((pA[0]*pB[1] + pB[0]*pC[1] + pC[0]*pA[1] - pA[1]*pB[0] - pB[1]*pC[0] - pC[1]*pA[0]) // 2)
+        cand_area = tri_area(self.intop, self.inlef, pt) +\
+                tri_area(self.intop, self.inrig, pt) +\
+                tri_area(self.inbot, self.inrig, pt) +\
+                tri_area(self.inbot, self.inlef, pt)
+        return self.area == cand_area
 
     def _seg_inter(self, seg1, seg2):
-        (A,B),(C,D) = seg1, seg2
-        if not self.isec(A,B,C,D):
-            return None
-        dx = (seg1[0][0] - seg1[1][0], seg2[0][0] - seg2[1][0])
-        dy = (seg1[0][1] - seg1[1][1], seg2[0][1] - seg2[1][1])
-        print((dx, dy))
-        div = self.det(dx, dy)
-        print(div)
-        if div == 0:
-            return None
-        d = (self.det(*seg1), self.det(*seg2))
-        x = self.det(d, dx) // div
-        y = self.det(d, dy) // div
-        return (x, y)
+        (p0, p1), (p2, p3) = seg1, seg2
+        s10x = p1[0] - p0[0]
+        s10y = p1[1] - p0[1]
+        s32x = p3[0] - p2[0]
+        s32y = p3[1] - p2[1]
+
+        den = s10x * s32y - s32x * s10y
+
+        if den == 0:
+            return None  # Collinear
+
+        is_pos = den > 0
+
+        s02x = p0[0] - p2[0]
+        s02y = p0[1] - p2[1]
+
+        s = s10x * s02y - s10y * s02x
+        if (s < 0) == is_pos:  return None
+        t = s32x * s02y - s32y * s02x
+        if (t < 0) == is_pos:  return None
+        if (s > den) == is_pos or (t > den) == is_pos:  return None
+        t /= den
+        return tuple(map(int, (p0[0] + t*s10x, p0[1] + t*s10y)))
 
     def ccw(self,A,B,C):
         return (C[1]-A[1]) * (B[0]-A[0]) > (B[1]-A[1]) * (C[1]-A[1])
@@ -62,54 +80,22 @@ class Endpoints:
 
     def intersection(self, other):
         inter_list = []
-        r = self._seg_inter((self.top, self.lef), (other.top, other.lef))  # UL vs UL
-        if r:
-            inter_list.append(r)
-        r = self._seg_inter((self.top, self.lef), (other.top, other.rig))  # UL vs UR
-        if r:
-            inter_list.append(r)
-        r = self._seg_inter((self.top, self.lef), (other.bot, other.rig))  # UL vs LR
-        if r:
-            inter_list.append(r)
-        r = self._seg_inter((self.top, self.lef), (other.bot, other.lef))  # UL vs LL
-        if r:
-            inter_list.append(r)
-        r = self._seg_inter((self.top, self.rig), (other.top, other.lef))  # UR vs UL
-        if r:
-            inter_list.append(r)
-        r = self._seg_inter((self.top, self.rig), (other.top, other.rig))  # UR vs UR
-        if r:
-            inter_list.append(r)
-        r = self._seg_inter((self.top, self.rig), (other.bot, other.rig))  # UR vs LR
-        if r:
-            inter_list.append(r)
-        r = self._seg_inter((self.top, self.rig), (other.bot, other.lef))  # UR vs LL
-        if r:
-            inter_list.append(r)
-        r = self._seg_inter((self.bot, self.lef), (other.top, other.lef))  # LR vs UL
-        if r:
-            inter_list.append(r)
-        r = self._seg_inter((self.bot, self.lef), (other.top, other.rig))  # LR vs UR
-        if r:
-            inter_list.append(r)
-        r = self._seg_inter((self.bot, self.lef), (other.bot, other.rig))  # LR vs LR
-        if r:
-            inter_list.append(r)
-        r = self._seg_inter((self.bot, self.lef), (other.bot, other.lef))  # LR vs LL
-        if r:
-            inter_list.append(r)
-        r = self._seg_inter((self.bot, self.rig), (other.top, other.lef))  # LL vs UL
-        if r:
-            inter_list.append(r)
-        r = self._seg_inter((self.bot, self.rig), (other.top, other.rig))  # LL vs UR
-        if r:
-            inter_list.append(r)
-        r = self._seg_inter((self.bot, self.rig), (other.bot, other.rig))  # LL vs LR
-        if r:
-            inter_list.append(r)
-        r = self._seg_inter((self.bot, self.rig), (other.bot, other.lef))  # LL vs LL
-        if r:
-            inter_list.append(r)
+        if r := self._seg_inter((self.top, self.lef), (other.top, other.lef)):  inter_list.append(r)  # UL vs UL
+        if r := self._seg_inter((self.top, self.lef), (other.top, other.rig)):  inter_list.append(r)  # UL vs UR
+        if r := self._seg_inter((self.top, self.lef), (other.bot, other.rig)):  inter_list.append(r)  # UL vs LR
+        if r := self._seg_inter((self.top, self.lef), (other.bot, other.lef)):  inter_list.append(r)  # UL vs LL
+        if r := self._seg_inter((self.top, self.rig), (other.top, other.lef)):  inter_list.append(r)  # UR vs UL
+        if r := self._seg_inter((self.top, self.rig), (other.top, other.rig)):  inter_list.append(r)  # UR vs UR
+        if r := self._seg_inter((self.top, self.rig), (other.bot, other.rig)):  inter_list.append(r)  # UR vs LR
+        if r := self._seg_inter((self.top, self.rig), (other.bot, other.lef)):  inter_list.append(r)  # UR vs LL
+        if r := self._seg_inter((self.bot, self.lef), (other.top, other.lef)):  inter_list.append(r)  # LR vs UL
+        if r := self._seg_inter((self.bot, self.lef), (other.top, other.rig)):  inter_list.append(r)  # LR vs UR
+        if r := self._seg_inter((self.bot, self.lef), (other.bot, other.rig)):  inter_list.append(r)  # LR vs LR
+        if r := self._seg_inter((self.bot, self.lef), (other.bot, other.lef)):  inter_list.append(r)  # LR vs LL
+        if r := self._seg_inter((self.bot, self.rig), (other.top, other.lef)):  inter_list.append(r)  # LL vs UL
+        if r := self._seg_inter((self.bot, self.rig), (other.top, other.rig)):  inter_list.append(r)  # LL vs UR
+        if r := self._seg_inter((self.bot, self.rig), (other.bot, other.rig)):  inter_list.append(r)  # LL vs LR
+        if r := self._seg_inter((self.bot, self.rig), (other.bot, other.lef)):  inter_list.append(r)  # LL vs LL
         return inter_list
 
     def det(self, dx, dy):
@@ -119,6 +105,7 @@ class Endpoints:
 v = ['',] * 10
 x_reject_segs = []
 endpoints_list = []
+beacon = None
 with open('input15', 'r') as file:
     for line in file:
         for i, word in enumerate(line.split()):
@@ -136,15 +123,15 @@ with open('input15', 'r') as file:
 i_list = []
 for i, e0 in enumerate(endpoints_list[:-1]):
     for j, e1 in enumerate(endpoints_list[i+1:], i+1):
-        print('---')
-        print((i,j))
-        print(e0)
-        print(e1)
         i_list.append(e0.intersection(e1))
 flattened = [x for y in i_list for x in y]
 flattened.sort()
 for pt in flattened:
-    print(pt)
+    if pt[0] < 0 or pt[1] < 0 or pt[0] > 4000000 or pt[1] > 4000000:
+        continue
+    if not any([e.check_point_contained(pt) for e in endpoints_list]):
+        beacon = pt
+        break
 
 x_reject_segs.sort()
 x_reject = [x_reject_segs[0]]
@@ -159,4 +146,4 @@ for seg in x_reject_segs:
 rejected = sum([e - s for s, e in x_reject])
 
 print(f'Part A: {rejected}')
-print(f'Part B: {0}')
+print(f'Part B: {4000000*beacon[0]+beacon[1]}')
