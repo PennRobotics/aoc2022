5e2a2835818cc997eb75b0617492a7df8fb250db
Brian Wright <4408242+PennRobotics@users.noreply.github.com>
2022-12-16 10:35:00 +0100

    add template for day 16

---
+++ day16.py
@@ -0,0 +1,18 @@
+_ = lambda: None
+
+class Endpoints:
+    def __init__(self):
+        self._ = None
+        pass
+
+    def __str__(self):
+        return f''
+
+
+v = 0
+with open('input16', 'r') as file:
+    for line in file:
+        pass
+
+print(f'Part A: {0}')
+print(f'Part B: {0}')
