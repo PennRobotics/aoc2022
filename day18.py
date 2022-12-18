6731151ac63842d3ddf51dbcfb31924bf38e02fe
Brian Wright <4408242+PennRobotics@users.noreply.github.com>
2022-12-18 14:53:00 +0100

    replace max/min with sorts

--- day18.py
+++ day18.py
@@ -1,20 +1,23 @@
 DEBUG = print if True else lambda *s: None
 
-voxels = dict()
+# Input data ranges: x = 1--19, y = 0--19, z = 0--18
+voxels = []
 with open('input18', 'r') as file:
-    min_x, min_y, min_z = 0, 0, 0
-    max_x, max_y, max_z = 0, 0, 0
+    cc = 0
     for line in file:
+        cc += 1  # TODO-debug
         x, y, z = map(int, line.rstrip('\n').split(','))
-        DEBUG((x,y,z))
-        min_x, max_x = min(min_x, x), max(max_x, x)
-        min_y, max_y = min(min_y, y), max(max_y, y)
-        min_z, max_z = min(min_z, z), max(max_z, z)
-        break
+        voxels.append((x,y,z),)
+        if cc == 10:
+            break
 
-DEBUG((min_x, max_x))
-DEBUG((min_y, max_y))
-DEBUG((min_z, max_z))
+DEBUG(voxels)
+
+sort(voxels, key=voxels[0])
+DEBUG(voxels)
+
+sort(voxels, key=voxels[1])
+DEBUG(voxels)
 
 print(f'Part A: {0}')
 print(f'Part B: {0}')
