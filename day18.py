7856cc8261f11156131495996f8c5c9107dbb32a
Brian Wright <4408242+PennRobotics@users.noreply.github.com>
2022-12-18 16:10:47 +0100

    submit Part A solution

--- day18.py
+++ day18.py
@@ -5,29 +5,38 @@ from operator import sub
 
 adj = lambda v1, v2: True if sum(map(abs, map(sub, v1, v2))) == 1 else False
 
-DEBUG(adj([0,1,1],[1,0,1]))  # Expect false
-DEBUG(adj([1,1,1],[1,0,1]))  # Expect true
-
 # Input data ranges: x = 1--19, y = 0--19, z = 0--18
 voxels = []
 with open('input18', 'r') as file:
-    cc = 0  # TODO-debug
     for line in file:
-        cc += 1
         x, y, z = map(int, line.rstrip('\n').split(','))
         voxels.append((x,y,z),)
-        if cc == 6:
-            break
 
 sa = 6 * len(voxels)
 
 voxels.sort(key=lambda e:e[0])
-DEBUG(voxels)
-for v, g in groupby(voxels, key=lambda e:e[0]):
-    print(v, g)
-    for vox in g:
-        print(vox)
+for _, group in groupby(voxels, key=lambda e:e[0]):
+    g = sorted(group, key=lambda e:e[1])
+    for _, group2 in groupby(g, key=lambda e:e[1]):
+        g2 = sorted(group2, key=lambda e:e[2])
+        for v1, v2 in zip(g2, g2[1:]):
+            sa -= 2 if adj(v1, v2) else 0
 
+voxels.sort(key=lambda e:e[1])
+for _, group in groupby(voxels, key=lambda e:e[1]):
+    g = sorted(group, key=lambda e:e[2])
+    for _, group2 in groupby(g, key=lambda e:e[2]):
+        g2 = sorted(group2, key=lambda e:e[0])
+        for v1, v2 in zip(g2, g2[1:]):
+            sa -= 2 if adj(v1, v2) else 0
 
-print(f'Part A: {0}')
+voxels.sort(key=lambda e:e[2])
+for _, group in groupby(voxels, key=lambda e:e[2]):
+    g = sorted(group, key=lambda e:e[0])
+    for _, group2 in groupby(g, key=lambda e:e[0]):
+        g2 = sorted(group2, key=lambda e:e[1])
+        for v1, v2 in zip(g2, g2[1:]):
+            sa -= 2 if adj(v1, v2) else 0
+
+print(f'Part A: {sa}')
 print(f'Part B: {0}')
