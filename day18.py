ae94a69cba3d948e666e076ff6eecb32089d5eb2
Brian Wright <4408242+PennRobotics@users.noreply.github.com>
2022-12-18 15:16:41 +0100

    add adjacency check and groupby

--- day18.py
+++ day18.py
@@ -1,5 +1,12 @@
 DEBUG = print if True else lambda *s: None
 
+from itertools import groupby
+from operator import sub
+
+adj = lambda v1, v2: map(sub, v1, v2)
+
+DEBUG(list(adj([5,3,2],[1,2,0])))  # Expect [4,1,2]
+
 # Input data ranges: x = 1--19, y = 0--19, z = 0--18
 voxels = []
 with open('input18', 'r') as file:
@@ -15,7 +22,11 @@ sa = 6 * len(voxels)
 
 voxels.sort(key=lambda e:e[0])
 DEBUG(voxels)
-DEBUG(*voxels)
+for v, g in groupby(voxels, key=lambda e:e[0]):
+    print(v, g)
+    for vox in g:
+        print(vox)
+
 
 print(f'Part A: {0}')
 print(f'Part B: {0}')
