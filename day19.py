3f5ee057b171393365fdc1e8c0406b0184823468
Brian Wright <4408242+PennRobotics@users.noreply.github.com>
2022-12-19 11:41:35 +0100

    add day 19 template

---
+++ day19.py
@@ -0,0 +1,8 @@
+DEBUG = print if False else lambda *s: None
+
+#with open('sample19', 'r') as file:
+with open('input19', 'r') as file:
+    c = file.read()
+
+print(f'Part A: {0}')
+print(f'Part B: {0}')
