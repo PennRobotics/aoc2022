35e787b0a8f9097187de2dfcd6e3eafed4e322b8
Brian Wright <4408242+PennRobotics@users.noreply.github.com>
2022-12-20 22:22:22 +0100

    add doubly linked list

--- day20.py
+++ day20.py
@@ -1,13 +1,83 @@
 DEBUG = print if True else lambda *s: None
 
+class Node:
+    def __init__(self, val, pr=None, nx=None, head=False):
+        global global_head
+        self.val = val
+        self.pr = pr
+        self.nx = nx
+        if not head:
+            self.head = global_head
+        else:
+            global_head = self
+
+    def jump_by_n(self, n):
+        travel_node = self
+        if n > 0:
+            for _ in range(n):
+                travel_node = travel_node.nx
+        if n < 0:
+            for _ in range(-n):
+                travel_node = travel_node.pr
+        move_node = self._pop_node()
+        travel_node._insert_node_after(move_node)
+
+    def redefine_next_as(self, nx):
+        self.nx = nx
+        nx.pr = self
+
+    def _pop_node(self):
+        bf_node, af_node = self.pr, self.nx
+        bf_node.redefine_next_as(af_node)
+        self.nx, self.pr = None, None
+        return self
+
+    def _insert_node_after(self, node):
+        af_node = self.nx
+        self.redefine_next_as(node)
+        node.redefine_next_as(af_node)
+
+
+def print_node_segment(i, j):
+    global global_head
+    ni = global_head
+    for _ in range(i):
+        ni = ni.nx
+    print('[', end='')
+    nj = ni
+    for _ in range(j-i):
+        print(f'{nj.val}, ', end='')
+        nj = nj.nx
+    print(f'{nj.val}]', flush=True)
+
 with open('sample20', 'r') as file:
 #with open('input20', 'r') as file:
-    blueprints = list(enumerate([int(n) for n in file.read().rstrip('\n').split('\n')]))
+    values = [int(n) for n in file.read().rstrip('\n').split('\n')]
+print(values)
 
-DEBUG(blueprints[:])
+global_head = None
+first_n = Node(values[0])
+last_n = first_n
+docket = [first_n]
+for v in values[1:]:
+    nv = Node(v, head=(v==0))
+    last_n.redefine_next_as(nv)
+    docket.append(nv)
+    last_n = nv
+last_n.redefine_next_as(first_n)
 
-import sys
-sys.exit(0)
+print_node_segment(2, 8)
+
+first_n.jump_by_n(1)
+first_n.pr.jump_by_n(2)
+
+print_node_segment(2, 8)
+
+#for mvmt in docket:
+#    mvmt.jump_by_n(mvmt.val)
+
+
+
 
 # In the naive implementation, a giant list is shifted from side-to-side, and the list
 # members have a reference/key that determines which element is shifted next.
