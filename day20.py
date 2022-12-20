37b042017d269b01f286593359e51f9897695c03
Brian Wright <4408242+PennRobotics@users.noreply.github.com>
2022-12-20 20:40:45 +0100

    add excessive thoughts and template for day 20

---
+++ day20.py
@@ -0,0 +1,92 @@
+DEBUG = print if True else lambda *s: None
+
+with open('sample20', 'r') as file:
+#with open('input20', 'r') as file:
+    blueprints = list(enumerate([int(n) for n in file.read().rstrip('\n').split('\n')]))
+
+DEBUG(blueprints[:])
+
+import sys
+sys.exit(0)
+
+# In the naive implementation, a giant list is shifted from side-to-side, and the list
+# members have a reference/key that determines which element is shifted next.
+# Not only would this be a bit of a pain to program, it's computationally inefficient.
+# Is there a way to create a "jump list" where the indices of each element are sorted
+# and stored? Then, when compiling the final version of the list (or an intermediate
+# list, if that is much more efficient or understandable) the elements are placed in
+# the correct place.
+
+# Example
+# =======
+#
+# -5,  7,  2,  4,  0,  0,  0,  0,  1,  0
+#
+#  (0, -5)  -> (0, 5) = (0, 0-5+10)
+#  (1,  7)  -> (1, 8)
+#  (2,  2)  -> (2, 4)
+#  (3,  4)  -> (3, 7)
+#  (4,  0)  -> (4, 4)
+#  (5,  0)  -> (5, 5)
+#  (6,  0)  -> (6, 6)
+#  (7,  0)  -> (7, 7)
+#  (8,  1)  -> (8, 9)
+#  (9,  0)  -> (9, 9)
+
+# Next step
+#  d   .   .   .   .   d   d   d   d   d
+#  0,  7,  2,  4,  0, -5,  0,  0,  0,  1
+# -5 jumps (0, 5)
+
+#  .   d   d   d   d   d   d   d   d   .
+#  0,  2,  4,  0, -5,  0,  0,  0,  7,  1
+#  7 jumps (1, 8)
+
+#  .   d   d   d   .   .   .   .   .   .
+#  0,  4,  0,  2, -5,  0,  0,  0,  7,  1
+#  2 jumps (1, 3)
+
+#  .   d   d   d   d   d   .   .   .   .
+#  0,  0,  2, -5,  0,  4,  0,  0,  7,  1
+#  4 jumps (1, 5)
+
+#  d   .   .   .   .   .   .   .   .   d
+#  1,  0,  2, -5,  0,  4,  0,  0,  7,  0
+#  1 jumps (9, 0)
+
+#  Included range and shift direction? (and how to deal with shift indices shifting??)
+
+#  a   0   0   0   0  +1  +1  +1  +1  +1
+#  0   b  -1  -1  -1  -1  -1  -1  -1   0
+#  0   c  -1  -1   0   0   0   0   0   0
+#  0   d  -1  -1  -1  -1   0   0   0   0
+# -1   0   0   0   0   0   0   0   0   e
+
+# ======================================
+# -1   0  -3  -3  -2  -1   0   0   0  +1
+
+# -5,  7,  2,  4,  0,  0,  0,  0,  1,  0
+#  1,  0,  2, -5,  0,  4,  0,  0,  7,  0
+
+# Ground truth:
+#  -5  -> (0, -7) / (0, 3)  = -7 (-2)
+#   7  -> (1,  8)           =  7 ( 0)
+#   2  -> (2,  2)           =  0 (-2)
+#   4  -> (3,  5)           =  2 (-2)
+#   0  -> (4,  1)           = -3 (-3)
+#   0  -> (5,  4)           = -1 (-1)
+#   0  -> (6,  6)           =  0 ( 0)
+#   0  -> (7,  7)           =  0 ( 0)
+#   1  -> (8,  0) / (8,10)  =  2 (+1)
+#   0  -> (9,  9)           =  0 ( 0)
+#
+# One final thought: The last list element is always going to skip the correct number
+# of other elements. Unfortunately, the second-to-last could be off by one without
+# knowing the start or stop point of the last element. By this point, the entire
+# landscape of values is completely different than in the beginning, so there's not
+# a great way I can think of to immediately know where the last element should start
+# or stop.
+
+
+print(f'Part A: {0}')
+print(f'Part B: {0}')
