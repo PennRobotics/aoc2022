27f7d141feef61510dec067c5e0ecee9d9b806b9
Brian Wright <4408242+PennRobotics@users.noreply.github.com>
2022-12-22 13:59:58 +0100

    add comments and structure for 3d "next movement" dict

--- day22.py
+++ day22.py
@@ -79,10 +79,98 @@ for turn, walk in path:
 DEBUG('\n'.join([r + '|' for r in maze]))
 DEBUG('----', flush=True)
 
-DEBUG(len(maze))
-DEBUG(len(maze[0]))
+DEBUG(len(maze))  # 201
+DEBUG(len(maze[0]))  # 151
+
+#             v c=150
+# o   +---+---x
+#     | B | A |
+#     +---z---+
+#     | C |
+# +---y---+
+# | E | D |
+# +---+---x
+# | F |
+# +---x  < r = 200
+
+# Edge A Direction 0 -> D Direction 2
+# Edge A Direction 1 -> C Direction 2
+# Edge A Direction 2 -> B Direction 2
+# Edge A Direction 3 -> F Direction 3
+# Edge B Direction 0 -> A Direction 0
+# Edge B Direction 1 -> C Direction 1
+# Edge B Direction 2 -> E Direction 0
+# Edge B Direction 3 -> F Direction 0
+# Edge C Direction 0 -> A Direction 3
+# Edge C Direction 1 -> D Direction 1
+# Edge C Direction 2 -> E Direction 1
+# Edge C Direction 3 -> B Direction 3
+# Edge D Direction 0 -> A Direction 2
+# Edge D Direction 1 -> F Direction 2
+# Edge D Direction 2 -> E Direction 2
+# Edge D Direction 3 -> C Direction 3
+# Edge E Direction 0 -> D Direction 0
+# Edge E Direction 1 -> F Direction 1
+# Edge E Direction 2 -> B Direction 0
+# Edge E Direction 3 -> C Direction 0
+# Edge F Direction 0 -> D Direction 3
+# Edge F Direction 1 -> A Direction 1
+# Edge F Direction 2 -> B Direction 1
+# Edge F Direction 3 -> E Direction 3
+
+# TODO: Coordinate reversals @ x, y, z? (For instance, A going RIGHT or D going RIGHT both exit with rotational symmetry)
 
 # TODO: create a jump map for all r,c,d returning next r,c,d
+# Top and bottom edge of upper-half faces (while flat, e.g. face A, B, and C)
+for jc in range(50,100):  # B, C
+    jr = 0
+    # TODO
+    jr = 49
+    # TODO
+    jr = 50
+    jr = 99
+    pass
+for jc in range(100,150):  # A
+    jr = 0
+    jr = 49
+    # TODO
+    pass
+# Vertical-while-flat edges of top faces
+for jr in range(0,50):  # A, B
+    jc = 50
+    jc = 99
+    jc = 100
+    jc = 149  # 10/24
+    # ...
+    pass
+# Vertical edges of face C
+for jr in range(50,100):
+    jc = 50
+    jc = 99  # 12/24
+    pass
+# Top and bottom horizontal-while-flat edges of faces F, E, and D
+for jc in range(50,100):  # D
+    jr = 100
+    jr = 149
+    pass
+for jc in range(0,50):  # E, F
+    jr = 100
+    jr = 149
+    jr = 150
+    jr = 199  # 18/24
+    pass
+# Vertical-while-flat edges of faces F, E, and D
+for jr in range(100,150):  # D, E
+    jc = 0
+    jc = 49
+    jc = 50
+    jc = 99  # 22/24
+    pass
+for jr in range(150,200):  # F
+    jc = 0
+    jc = 49
+    pass
+
 
 print(f'Part A: {1000*r + 4*c + d}')
 print(f'Part B: {0}')
