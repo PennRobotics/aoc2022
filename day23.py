55eb99d47ddd78d5d20c453a6fdae715e94bbbac
Brian Wright <4408242+PennRobotics@users.noreply.github.com>
2022-12-23 16:20:58 +0100

    upload solution to part b

--- day23.py
+++ day23.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+from itertools import count
 
 is_north_empty = lambda p: (p[0]-1, p[1]-1) not in elves and (p[0]-1, p[1]) not in elves and (p[0]-1, p[1]+1) not in elves
 is_south_empty = lambda p: (p[0]+1, p[1]-1) not in elves and (p[0]+1, p[1]) not in elves and (p[0]+1, p[1]+1) not in elves
@@ -13,6 +14,7 @@ east_of = lambda p: (p[0], p[1]+1)
 
 heading_at_round_start = 1j
 
+#with open('sample23','r') as file:
 with open('input23','r') as file:
     puzzle = [line[:-1] for line in file]
 ROW_MIN, ROW_MAX, COL_MIN, COL_MAX = 0, len(puzzle)-1, 0, len(puzzle[0])-1
@@ -32,10 +34,11 @@ def draw_board():
         print()
     print(flush=True)
 
-print('== Initial State ==')
-draw_board()
+#print('== Initial State ==')
+#draw_board()
 
-for round_nr, round_dir in enumerate([1j**i for i in range(10)], 1):
+for round_nr in count(1):
+    round_dir = 1j**((round_nr-1)%4)
     proposed_moves = defaultdict(lambda:[])
     for elf in sorted(elves):
         if is_perimeter_empty(elf):
@@ -67,15 +70,15 @@ for round_nr, round_dir in enumerate([1j**i for i in range(10)], 1):
         else:
             next_elves |= set([pmove,])
 
-    if all([pmove in movers for pmove, movers in proposed_moves.items()]):
-        print('Early finish!')
+    if elves == next_elves:
         break
 
     elves = next_elves
-    print(f'== End of Round {round_nr} ==')
-    draw_board()
+    #print(f'== End of Round {round_nr} ==')
+    #draw_board()
 
-r1, r2, c1, c2 = get_row_col_bounds()
+    if round_nr == 10:
+        r1, r2, c1, c2 = get_row_col_bounds()
+        print(f'Part A: {(r2-r1+1)*(c2-c1+1)-len(elves)}')
 
-print(f'Part A: {(r2-r1+1)*(c2-c1+1)-len(elves)}')
-print(f'Part B: {0}')
+print(f'Part B: {round_nr}')  # 102 is too low
