251965e8136677469c7a5858f3a1d3cfe0b4c265
Brian Wright <4408242+PennRobotics@users.noreply.github.com>
2022-12-25 12:09:27 +0100

    fix bugs (now compiles but hangs)

--- day25.c
+++ day25.c
@@ -1,15 +1,21 @@
 #include <math.h>
 #include <stdint.h>
 #include <stdio.h>
+// #include <stdlib.h>  // TODO: check if needed
 
 #define  DEBUG(s,v)  do{if(1){printf(s, v);}}while(0)
 
 int main() {
-  FILE *fh = open("input25", "r");
+  char *line;
+  FILE *fh = fopen("input25", "r");
   while(fh) {
     uint64_t n = 0;
+    size_t len;
+    getline(&line, &len, fh);
+    char ch;
     do {
-      char ch = fh->readbytes();  // TODO: store as char array and reverse order
+      uint8_t i;
+      ch = line[i];
       uint8_t place = 0;
       uint64_t val;
       switch(ch) {
@@ -29,10 +35,12 @@ int main() {
           val = 2;
           break;
       }
-      n += powl(5, place) * val;
+      n += (int)pow(5, place) * val;
     } while (ch != '\n');
     DEBUG("%lld\n", n);
   }
+
+  fclose(fh);
 
   printf("Part A: %lld\n", 0);
   printf("Part B: %lld\n", 0);
