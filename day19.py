e3102f3c91d9021ba751b712cd9e898450fae609
Brian Wright <4408242+PennRobotics@users.noreply.github.com>
2022-12-27 00:22:45 +0100

    rearrange build logic

--- day19.py
+++ day19.py
@@ -15,47 +15,45 @@ with open('input19', 'r') as file:
 
 blueprints = [blueprints[0]]  # TODO-debug
 
+t_to_build = lambda cost, cur, rate: None  # TODO
+
 quality_level = 0
 for b, co1, co2, co3, cc3, co4, cx4 in blueprints:
     o, c, x, g, r1, r2, r3, r4 = 0, 0, 0, 0, 1, 0, 0, 0
     t = 0
     possible_states = [[t, o, c, x, g, r1, r2, r3, r4]]
-    while t < 24:
-        DEBUG(f'== Minute {t} ==', flush=True)
+    for state in possible_states:  # t, o, c, x, g, r1, r2, r3, r4 = state
+        if t >= 24:
+            break
 
-        new_states = []
-        for ostate in possible_states:
-            state = copy(ostate)
-            _, o, c, x, g, r1, r2, r3, r4 = state
+        if x >= cx4 and o >= co4:  # Geode
+            state[8] += 1
+            state[1] -= co4
+            state[3] -= cx4
+            bt = max(t_to_build(cx4, x, r3), t_to_build(co4, o, r1))
+        elif c >= cc3 and o >= co3 and r3 < cx4:  # Obsidian
+            state[7] += 1
+            state[1] -= co3
+            state[2] -= cc3
+            bt = max(t_to_build(cc3, c, r2), t_to_build(co3, o, r1))
+        elif o >= co2 and r2 < cc3:  # Clay
+            state[6] += 1
+            state[1] -= co2
+            bt = t_to_build(co2, o, r1)
+        else:  # Ore
+            if o >= co1 and r1 < max(co1, co2, co3, co4):  # Saturation (cannot create more than one bot per turn for any material)
+                state[5] += 1
+                state[1] -= co1
+                bt = t_to_build(co1, o, r1)
+        bt = 0  # TODO: bt needs to be properly assigned. rethink if-elif-else order!
 
-            state[0] = t
-            state[1] += r1
-            state[2] += r2
-            state[3] += r3
-            state[4] += r4
+        state[0] = t + bt
+        state[1] += r1 * bt
+        state[2] += r2 * bt
+        state[3] += r3 * bt
+        state[4] += r4 * bt
 
-            if x >= cx4 and o >= co4:  # Geode
-                state[8] += 1
-                o -= co4
-                state[1] -= co4
-                x -= cx4
-                state[3] -= cx4
-            if c >= cc3 and o >= co3 and r3 < cx4:  # Obsidian
-                state[7] += 1
-                o -= co3
-                state[1] -= co3
-                c -= cc3
-                state[2] -= cc3
-            if o >= co2 and r2 < cc3:  # Clay
-                state[6] += 1
-                o -= co2
-                state[1] -= co2
-            if co1 <= co2:  # Ore
-                if o >= co1 and r1 < max(co1, co2, co3, co4):  # Saturation (cannot create more than one bot per turn for any material)
-                    state[5] += 1
-                    o -= co1
-                    state[1] -= co1
-            possible_states.append(state)
+        possible_states = [state] + possible_states
 
     # TODO: check if the candidate state is feasible (can a geode robot be build by 23m?) or even optimal (how many geode robots COULD be built given a set of inputs?)
     # TODO: choose top state, go to next blueprint
