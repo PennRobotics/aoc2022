5d04eb4ffd48e2f8be83e3b642ec1cc4366b35b9
Brian Wright <4408242+PennRobotics@users.noreply.github.com>
2022-12-27 00:39:38 +0100

    call search from each path

--- day19.py
+++ day19.py
@@ -3,12 +3,6 @@ DEBUG = print if True else lambda *s: None
 import math
 import re
 
-NOTHING        = 0
-ORE_ROBOT      = 1
-CLAY_ROBOT     = 2
-OBSIDIAN_ROBOT = 4
-GEODE_ROBOT    = 8
-
 #with open('sample19', 'r') as file:
 with open('input19', 'r') as file:
     blueprints = [map(int, re.findall(r'\d+', l)) for l in file.read().rstrip('\n').split('\n')]
@@ -25,31 +19,44 @@ def search(state):
         state[1] -= co4
         state[3] -= cx4
         bt = max(t_to_build(cx4, x, r3), t_to_build(co4, o, r1))
-    elif c >= cc3 and o >= co3 and r3 < cx4:  # Obsidian
+        state[0] = t + bt
+        state[1] += r1 * bt
+        state[2] += r2 * bt
+        state[3] += r3 * bt
+        state[4] += r4 * bt
+        search(state)
+    if c >= cc3 and o >= co3 and r3 < cx4:  # Obsidian
         state[7] += 1
         state[1] -= co3
         state[2] -= cc3
         bt = max(t_to_build(cc3, c, r2), t_to_build(co3, o, r1))
-    elif o >= co2 and r2 < cc3:  # Clay
+        state[0] = t + bt
+        state[1] += r1 * bt
+        state[2] += r2 * bt
+        state[3] += r3 * bt
+        state[4] += r4 * bt
+        search(state)
+    if o >= co2 and r2 < cc3:  # Clay
         state[6] += 1
         state[1] -= co2
         bt = t_to_build(co2, o, r1)
-    else:  # Ore
-        if o >= co1 and r1 < max(co1, co2, co3, co4):  # Saturation (cannot create more than one bot per turn for any material)
-            state[5] += 1
-            state[1] -= co1
-            bt = t_to_build(co1, o, r1)
-    bt = 1  # TODO: bt needs to be properly assigned. rethink if-elif-else order!
+        state[0] = t + bt
+        state[1] += r1 * bt
+        state[2] += r2 * bt
+        state[3] += r3 * bt
+        state[4] += r4 * bt
+        search(state)
+    if o >= co1 and r1 < max(co1, co2, co3, co4):  # Saturation (cannot create more than one bot per turn for any material)
+        state[5] += 1
+        state[1] -= co1
+        bt = t_to_build(co1, o, r1)
+        state[0] = t + bt
+        state[1] += r1 * bt
+        state[2] += r2 * bt
+        state[3] += r3 * bt
+        state[4] += r4 * bt
+        search(state)
 
-    state[0] = t + bt
-    state[1] += r1 * bt
-    state[2] += r2 * bt
-    state[3] += r3 * bt
-    state[4] += r4 * bt
-
-    DEBUG(state[0])
-
-    return search(state)
 
 quality_level = 0
 for b, co1, co2, co3, cc3, co4, cx4 in blueprints:
