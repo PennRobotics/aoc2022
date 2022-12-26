53c7bbda69e32a5581ff67934e023061e5f1e652
Brian Wright <4408242+PennRobotics@users.noreply.github.com>
2022-12-27 00:50:42 +0100

    unpacked state to avoid shallow copy problems

--- day19.py
+++ day19.py
@@ -11,60 +11,58 @@ blueprints = [blueprints[0]]  # TODO-debug
 
 t_to_build = lambda cost, cur, rate: math.ceil((cost-cur)/rate)
 
-def search(state):
-    print(state)
+def search(t, o, c, x, g, r1, r2, r3, r4):
     if t >= 24:
-        return state[4]
+        return g
     if x >= cx4 and o >= co4:  # Geode
-        state[8] += 1
-        state[1] -= co4
-        state[3] -= cx4
+        r4 += 1
+        o -= co4
+        x -= cx4
         bt = max(t_to_build(cx4, x, r3), t_to_build(co4, o, r1))
-        state[0] = t + bt
-        state[1] += r1 * bt
-        state[2] += r2 * bt
-        state[3] += r3 * bt
-        state[4] += r4 * bt
-        search(state)
+        t = t + bt
+        o += r1 * bt
+        c += r2 * bt
+        x += r3 * bt
+        g += r4 * bt
+        search(t, o, c, x, g, r1, r2, r3, r4)
     if c >= cc3 and o >= co3 and r3 < cx4:  # Obsidian
-        state[7] += 1
-        state[1] -= co3
-        state[2] -= cc3
+        r3 += 1
+        o -= co3
+        c -= cc3
         bt = max(t_to_build(cc3, c, r2), t_to_build(co3, o, r1))
-        state[0] = t + bt
-        state[1] += r1 * bt
-        state[2] += r2 * bt
-        state[3] += r3 * bt
-        state[4] += r4 * bt
-        search(state)
+        t = t + bt
+        o += r1 * bt
+        c += r2 * bt
+        x += r3 * bt
+        g += r4 * bt
+        search(t, o, c, x, g, r1, r2, r3, r4)
     if o >= co2 and r2 < cc3:  # Clay
-        state[6] += 1
-        state[1] -= co2
+        r2 += 1
+        o -= co2
         bt = t_to_build(co2, o, r1)
-        state[0] = t + bt
-        state[1] += r1 * bt
-        state[2] += r2 * bt
-        state[3] += r3 * bt
-        state[4] += r4 * bt
-        search(state)
+        t = t + bt
+        o += r1 * bt
+        c += r2 * bt
+        x += r3 * bt
+        g += r4 * bt
+        search(t, o, c, x, g, r1, r2, r3, r4)
     if r1 < max(co1, co2, co3, co4):  # Saturation (cannot create more than one bot per turn for any material)
-        state[5] += 1
-        state[1] -= co1
+        r1 += 1
+        o -= co1
         bt = t_to_build(co1, o, r1)
-        state[0] = t + bt
-        state[1] += r1 * bt
-        state[2] += r2 * bt
-        state[3] += r3 * bt
-        state[4] += r4 * bt
-        search(state)
+        t = t + bt
+        o += r1 * bt
+        c += r2 * bt
+        x += r3 * bt
+        g += r4 * bt
+        search(t, o, c, x, g, r1, r2, r3, r4)
 
 
 quality_level = 0
 for b, co1, co2, co3, cc3, co4, cx4 in blueprints:
     o, c, x, g, r1, r2, r3, r4 = 0, 0, 0, 0, 1, 0, 0, 0
     t = 0
-    state = [t, o, c, x, g, r1, r2, r3, r4]
-    search(state)
+    search(t, o, c, x, g, r1, r2, r3, r4)
 
 print(f'Part A: {0}')
 print(f'Part B: {0}')
